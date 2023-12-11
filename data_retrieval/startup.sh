39d56697e0d58b5c2292eeef764cb19d38db6691
Pedro Gomes <pedromgomes29@gmail.com>
2023-12-11 11:02:15 +0000

    Initial suggest conf

--- data_retrieval/startup.sh
+++ data_retrieval/startup.sh
@@ -51,6 +51,39 @@ curl -X POST -H 'Content-type:application/json' -d '{
 }' http://localhost:8983/solr/articles/config
 
 
+curl -X POST -H 'Content-type:application/json'  -d '{
+  "add-searchcomponent": {
+    "name": "suggest",
+    "class": "solr.SuggestComponent",
+    "suggester": {
+        "name": "mySuggester",
+        "lookupImpl": "FuzzyLookupFactory",
+        "dictionaryImpl": "DocumentDictionaryFactory",
+        "field": "title",
+        "suggestAnalyzerFieldType": "text_general",
+        "exactMatchFirst": "true",
+        "buildOnStartup": "true"
+    }
+  }
+}' http://localhost:8983/solr/articles/config
+
+
+curl -X POST -H 'Content-type:application/json'  -d '{
+  "add-requesthandler": {
+    "name": "/suggest",
+        "class": "solr.SearchHandler",
+        "startup": "lazy",
+        "defaults": {
+            "suggest": true,
+            "suggest.count": 20,
+            "suggest.dictionary": "mySuggester"
+        },
+        "components": [
+            "suggest"
+        ]
+  }
+}' http://localhost:8983/solr/articles/config
+
 
 # Schema definition via API
 curl -X POST -H 'Content-type:application/json' \
